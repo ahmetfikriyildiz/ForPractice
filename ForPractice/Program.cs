84e4ab7191eaf6c3761787674fc018397309992b
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-03 19:05:05 +0300

    Add project files.

---
+++ ForPractice/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int toplam1 = 0;
+            int toplam2 = 0;
+            #region 1->
+            //for (i = 0; i < 10; i++) 
+            //{
+            //    Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+            //}
+            #endregion
+            #region 2->
+            //for(i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region 3->
+            //for (i = 1; i <= 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region 4->
+            //for (i = 50; i <= 150 ; i++)
+            //{
+            //    toplam += i;
+            //}
+            //Console.WriteLine(toplam);
+            #endregion
+            #region 5->
+            //for(i = 1; i <= 120; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        toplam1 += i;
+            //    }
+            //    else
+            //    {
+            //        toplam2 += i;
+            //    }
+            //}
+            //Console.WriteLine(toplam1);
+            //Console.WriteLine(toplam2);
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
